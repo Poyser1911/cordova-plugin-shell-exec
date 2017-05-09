dd127434cd0a4f563812da3d34f612adbb650519
Poyser1911 <damanipoyser1911@yahoo.com>
2017-05-09 08:43:15 -0500

    Inital Commit
    Plugin Files

---
+++ README.md
@@ -0,0 +1,52 @@
+# cordova-plugin-shell-exec
+
+Apache Cordova Plugin to Execute Root Commands Android in Smartphone's Operating System Shell.
+
+
+This is a MODIFIED version of [https://github.com/petervojtek/cordova-plugin-shell-exec](https://github.com/petervojtek/cordova-plugin-shell-exec) to be complatible with all commands formats without using arrays, see `Useage` and `Examples`.
+Note: All Commands Excutes in Root Mode at the moment, this can be modified later.
+
+
+### Supported Platforms
+
+* Android
+
+### Installation
+
+`$ cordova plugin add https://github.com/Poyser1911/cordova-plugin-shell-exec.git`
+
+### Usage
+
+```
+window.ShellExec.exec(cmd, callback);
+```
+
+`cmd` : String.
+
+callback : function will return a object with two keys - `exitStatus` and `output`.
+
+Examples: `"Quotes Around Path are optional"`
+```
+var cmd = 'ls';
+cmd = 'ls /data';
+cmd = 'ls "/data/system\ prefs""; //escaped Whitespace
+
+cmd = 'ls && ls /data && cp "/data/data/com.example.app" "/data/data/com.example.app2" && rm -r -f  "/data/data/com.example.app2"'; //Using &&
+```
+
+```
+window.ShellExec.exec(cmd, (res) =>{
+  alert('exit status: ' + res.exitStatus)
+  alert('cmd output: ' + res.output)
+})
+```
+### Alerts Result Preview
+![alt text](http://poyser.pw/git/images/exitStatus.png"exitStatus")
+
+
+
+![alt text](http://poyser.pw/git/images/output.png"output")
+
+
+```
+
---
+++ src/org/apache/cordova/plugin/ShellExec.java
@@ -0,0 +1,72 @@
+package org.apache.cordova.plugin;
+
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.DataOutputStream;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class ShellExec extends CordovaPlugin {
+    @Override
+    public boolean execute(String action, JSONArray data, final CallbackContext callbackContext) throws JSONException {
+        if (action.equals("exec")) {
+            final String cmd = data.getString(0);
+            cordova.getThreadPool().execute(new Runnable() {
+                public void run() {
+                    Process p;
+                    StringBuffer output = new StringBuffer();
+                    int exitStatus = -1;
+                    try {
+                        p = Runtime.getRuntime().exec("su");
+                        DataOutputStream outputStream = new DataOutputStream(p.getOutputStream());
+
+                        outputStream.writeBytes(cmd + "\n");
+                        outputStream.flush();
+
+                        outputStream.writeBytes("exit\n");
+                        outputStream.flush();
+
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                        BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+                        String line = "";
+
+                        while ((line = reader.readLine()) != null) {
+                            output.append(line + "\n");
+                        }
+                        while ((line = stdError.readLine()) != null) {
+                            output.append(line);
+                        }
+                        exitStatus = p.waitFor();
+
+                    } catch (IOException e) {
+                        output.append("IOException: " + e.getMessage() + "\n");
+                    } catch (InterruptedException e) {
+                        output.append("InterruptedException: " + e.getMessage() + "\n");
+                    } catch (Exception e) {
+                        output.append("InterruptedException: " + e.getMessage() + "\n");
+                    }
+                    try {
+                        JSONObject json = new JSONObject();
+                        json.put("exitStatus", exitStatus);
+                        json.put("output", output.toString());
+                        callbackContext.success(json);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        callbackContext.success();
+                    }
+                }
+            });
+            return true;
+        }
+        return false;
+    }
+
+}
